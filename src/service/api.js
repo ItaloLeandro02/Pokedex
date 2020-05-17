f8318111284cb5dec2af5a55dceaef240e4af94a
Italo Leandro <leandroitalo433@gmail.com>
2020-05-17 00:25:22 -0300

    Configurando interceptors do axios

--- src/service/api.js
+++ src/service/api.js
@@ -1,9 +1,17 @@
 import axios from 'axios';
-
-import { BASE_URL } from 'react-native-dotenv';
+import { Alert } from 'react-native';
 
 const api = axios.create({
-    baseURL: BASE_URL
+  baseURL: 'https://api.pokemontcg.io/v1',
 });
+
+api.interceptors.response.use(
+  response => response,
+  err => {
+    Alert.alert(err.message);
+
+    return Promise.reject(err);
+  }
+);
 
 export default api;
