b1cd8787f8af354dfee52dddd1a080fe441566fb
Italo Leandro <leandroitalo433@gmail.com>
2020-05-17 00:25:01 -0300

    Adicionado arquivo com icones

---
+++ src/assets/Images/index.js
@@ -0,0 +1,25 @@
+import ColorLess from './colorless.png';
+import Darkness from './darkness.png';
+import Dragon from './dragon.png';
+import Fairy from './fairy.png';
+import Fighting from './fighting.png';
+import Fire from './fire.png';
+import Grass from './grass.png';
+import Lightning from './lightning.png';
+import Metal from './metal.png';
+import Psychic from './psychic.png';
+import Water from './water.png';
+
+export {
+  ColorLess,
+  Darkness,
+  Dragon,
+  Fairy,
+  Fighting,
+  Fire,
+  Grass,
+  Lightning,
+  Metal,
+  Psychic,
+  Water,
+};
