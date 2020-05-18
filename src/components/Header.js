7cd07deb80b45b37100022c03b856abbe0326173
Italo Leandro <leandroitalo433@gmail.com>
2020-05-17 21:43:32 -0300

    Modificado Header da aplicaçao

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,11 @@
 import React from 'react';
-import {Header, Left, Body, Right, Button, Icon, Title} from 'native-base';
+import { Header, Left, Body, Right, Button, Icon, Title } from 'native-base';
 
-const CustomHeader = ({isHome, navigation, title}) => {
+const CustomHeader = ({ isHome, navigation, title }) => {
   return (
     <Header>
       <Left>
-        {isHome ? (
-          <Button transparent>
-            <Icon name="menu" />
-          </Button>
-        ) : (
+        {!isHome && (
           <Button transparent onPress={() => navigation.goBack()}>
             <Icon name="arrow-back" />
           </Button>
