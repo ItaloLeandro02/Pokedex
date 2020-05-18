0406c1c641d1bbc9f1d0764dbb0f42313f7bef33
Italo Leandro <leandroitalo433@gmail.com>
2020-05-17 21:44:01 -0300

    O titulo do card passa a ser o nome do pokemon

--- src/pages/CardDetail/index.js
+++ src/pages/CardDetail/index.js
@@ -1,7 +1,8 @@
-import React from 'react';
+import React, { useLayoutEffect } from 'react';
 
 import Ability from '../../components/Ability';
 import Attack from '../../components/Attack';
+import Header from '../../components/Header';
 import {
   Attacks,
   Container,
@@ -21,7 +22,7 @@ import {
 } from './styles';
 import { ReturnIconCost } from '../../utils/CostIcons';
 
-const CardDetail = ({ route }) => {
+const CardDetail = ({ navigation, route }) => {
   const {
     card: {
       ability,
@@ -58,118 +59,121 @@ const CardDetail = ({ route }) => {
   }));
 
   return (
-    <Container>
-      <Image source={{ uri: imageUrlHiRes }} />
-      <Title>{name}</Title>
-      <SuperType>
-        {supertype} - <SubType>{subtype}</SubType>
-      </SuperType>
+    <>
+      <Header navigation={navigation} title={name} />
+      <Container>
+        <Image source={{ uri: imageUrlHiRes }} />
+        <Title>{name}</Title>
+        <SuperType>
+          {supertype} - <SubType>{subtype}</SubType>
+        </SuperType>
 
-      {hp && String(hp) !== String('None') && (
-        <Hp>
-          HP {hp}{' '}
-          {iconsType.map((icon, index) => (
-            <IconCost key={index} source={icon} />
-          ))}
-        </Hp>
-      )}
-
-      {ability && <Ability ability={ability} />}
-
-      {/* Rules */}
-      {text.length > 0 ? (
-        <>
-          <RulesTitle>Rules</RulesTitle>
-          {text.map((rule, index) => (
-            <RulesText key={index}>{rule}</RulesText>
-          ))}
-        </>
-      ) : null}
-
-      <Attacks
-        data={attacks}
-        keyExtractor={attack => String(attack.name)}
-        renderItem={({ item }) => <Attack attack={item} />}
-      />
-
-      {/* Weakness */}
-      <ContentProperties style={{ marginTop: 15 }}>
-        <TitleProperties>Weakness</TitleProperties>
-
-        {FormattedWeaknesses.length > 0 ? (
-          FormattedWeaknesses.map(({ icon, value }, index) => (
-            <ContentRow key={index}>
-              <IconCost source={icon} />
-              <ValueProperties>{value}</ValueProperties>
-            </ContentRow>
-          ))
-        ) : (
-          <NaText>n/a</NaText>
-        )}
-      </ContentProperties>
-
-      {/* Resistances */}
-      <ContentProperties>
-        <TitleProperties>Resistances</TitleProperties>
-        {FormattedResistances.length > 0 ? (
-          FormattedResistances.map(({ icon, value }, index) => (
-            <ContentRow key={index}>
-              <IconCost source={icon} />
-              <ValueProperties>{value}</ValueProperties>
-            </ContentRow>
-          ))
-        ) : (
-          <NaText>n/a</NaText>
-        )}
-      </ContentProperties>
-
-      {/* Retreat cost */}
-      <ContentProperties>
-        <TitleProperties>Retreat Cost</TitleProperties>
-        <ContentRow>
-          {iconsRetreatCost.length > 0 ? (
-            iconsRetreatCost.map((icon, index) => (
+        {hp && String(hp) !== String('None') && (
+          <Hp>
+            HP {hp}{' '}
+            {iconsType.map((icon, index) => (
               <IconCost key={index} source={icon} />
+            ))}
+          </Hp>
+        )}
+
+        {ability && <Ability ability={ability} />}
+
+        {/* Rules */}
+        {text.length > 0 ? (
+          <>
+            <RulesTitle>Rules</RulesTitle>
+            {text.map((rule, index) => (
+              <RulesText key={index}>{rule}</RulesText>
+            ))}
+          </>
+        ) : null}
+
+        <Attacks
+          data={attacks}
+          keyExtractor={attack => String(attack.name)}
+          renderItem={({ item }) => <Attack attack={item} />}
+        />
+
+        {/* Weakness */}
+        <ContentProperties style={{ marginTop: 15 }}>
+          <TitleProperties>Weakness</TitleProperties>
+
+          {FormattedWeaknesses.length > 0 ? (
+            FormattedWeaknesses.map(({ icon, value }, index) => (
+              <ContentRow key={index}>
+                <IconCost source={icon} />
+                <ValueProperties>{value}</ValueProperties>
+              </ContentRow>
             ))
           ) : (
             <NaText>n/a</NaText>
           )}
-        </ContentRow>
-      </ContentProperties>
+        </ContentProperties>
 
-      {/* Artist */}
-      <ContentProperties style={{ marginTop: 15 }}>
-        <TitleProperties>Artist</TitleProperties>
-        {artist ? (
-          <ValueProperties>{artist}</ValueProperties>
-        ) : (
-          <NaText>n/a</NaText>
-        )}
-      </ContentProperties>
+        {/* Resistances */}
+        <ContentProperties>
+          <TitleProperties>Resistances</TitleProperties>
+          {FormattedResistances.length > 0 ? (
+            FormattedResistances.map(({ icon, value }, index) => (
+              <ContentRow key={index}>
+                <IconCost source={icon} />
+                <ValueProperties>{value}</ValueProperties>
+              </ContentRow>
+            ))
+          ) : (
+            <NaText>n/a</NaText>
+          )}
+        </ContentProperties>
 
-      {/* Rarity */}
-      <ContentProperties>
-        <TitleProperties>Rarity</TitleProperties>
-        {rarity ? (
-          <ValueProperties>{rarity}</ValueProperties>
-        ) : (
-          <NaText>n/a</NaText>
-        )}
-      </ContentProperties>
+        {/* Retreat cost */}
+        <ContentProperties>
+          <TitleProperties>Retreat Cost</TitleProperties>
+          <ContentRow>
+            {iconsRetreatCost.length > 0 ? (
+              iconsRetreatCost.map((icon, index) => (
+                <IconCost key={index} source={icon} />
+              ))
+            ) : (
+              <NaText>n/a</NaText>
+            )}
+          </ContentRow>
+        </ContentProperties>
 
-      {/* Set */}
-      <ContentProperties>
-        <TitleProperties>Set</TitleProperties>
-        <ContentRow>
-          <ValueProperties>{set}</ValueProperties>
-          <IconCost
-            source={{
-              uri: `https://images.pokemontcg.io/${setCode}/symbol.png`,
-            }}
-          />
-        </ContentRow>
-      </ContentProperties>
-    </Container>
+        {/* Artist */}
+        <ContentProperties style={{ marginTop: 15 }}>
+          <TitleProperties>Artist</TitleProperties>
+          {artist ? (
+            <ValueProperties>{artist}</ValueProperties>
+          ) : (
+            <NaText>n/a</NaText>
+          )}
+        </ContentProperties>
+
+        {/* Rarity */}
+        <ContentProperties>
+          <TitleProperties>Rarity</TitleProperties>
+          {rarity ? (
+            <ValueProperties>{rarity}</ValueProperties>
+          ) : (
+            <NaText>n/a</NaText>
+          )}
+        </ContentProperties>
+
+        {/* Set */}
+        <ContentProperties>
+          <TitleProperties>Set</TitleProperties>
+          <ContentRow>
+            <ValueProperties>{set}</ValueProperties>
+            <IconCost
+              source={{
+                uri: `https://images.pokemontcg.io/${setCode}/symbol.png`,
+              }}
+            />
+          </ContentRow>
+        </ContentProperties>
+      </Container>
+    </>
   );
 };
 
